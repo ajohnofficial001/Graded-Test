913d754d1e2b9970ad61d1f448a546555f883e0e
John Adeyemo <ajohnofficial2020@gmail.com>
2023-03-26 20:52:30 +0100

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
           maxWidth="1024px"
           height={16}
         >
-          <p>John Adeyemo • © 2022</p>
+          <p>John Adeyemo • © 2023</p>
         </Flex>
       </footer>
     </Box>
